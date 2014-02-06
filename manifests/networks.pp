c1bcf2f7156f95258bd7ad841625aeb54a9adc3e
primeministerp <primeministerpete@gmail.com>
2014-02-06 09:33:10 -0500

    new class

--- manifests/networks.pp
+++ manifests/networks.pp
@@ -1,3 +1,14 @@
-define iphawk::networks ( $network_address, $netmask_cidr ) {
-} 
+class iphawk::network
+  define iphawk::network::gateway () {
+    concat::fragment{"additional_network_${name}":
+      content => "${name},",
+    }
+  } 
+
+  define iphawk::network::subnet ( $network_address, $netmask_cidr ) {
+    concat::fragment{"additional_network_${name}":
+      content => "${network_address}/${netmask_cidr},",
+    }
+  } 
+}
 
